e6aa91d74630733ae64247848042760e2bbac819
anouar <anwarwenners@gmail.com>
2025-07-01 13:29:58 +0100

    handl diff fonction

--- core/Vdom/diffing.js
+++ core/Vdom/diffing.js
@@ -1,59 +1,53 @@
-
-
-
-
-let globalIndex =0;
-
-
-
 export function diff(oldNode, newNode) {
   const patches = [];
+  let index = 0; 
 
-  walk(oldNode, newNode, patches, 0);
-   
+  walk(oldNode, newNode, patches, index);
+
   return patches;
 }
+
 function walk(oldNode, newNode, patches, index) {
   const currentPatch = [];
-           
-  if (!newNode) {                                                   
-    currentPatch.push({ type: 'REMOVE', index });
+
+  if (!newNode) {
+    currentPatch.push({ type: "REMOVE", index });
   } else if (!oldNode) {
-    currentPatch.push({ type: 'ADD', newNode });
+    currentPatch.push({ type: "ADD", newNode, index });
   } else if (isTextNode(oldNode) && isTextNode(newNode)) {
     if (oldNode.content !== newNode.content) {
-      currentPatch.push({ type: 'TEXT', content: newNode.content });
+      currentPatch.push({ type: "TEXT", content: newNode.content, index });
     }
   } else if (oldNode.tag === newNode.tag) {
     const attrPatches = diffAttrs(oldNode.attrs, newNode.attrs);
     if (Object.keys(attrPatches).length > 0) {
-      currentPatch.push({ type: 'ATTRS', attrs: attrPatches });
+      currentPatch.push({ type: "ATTRS", attrs: attrPatches, index });
     }
 
     diffChildren(oldNode.children || [], newNode.children || [], patches, index);
   } else {
-    currentPatch.push({ type: 'REPLACE', newNode });
+    currentPatch.push({ type: "REPLACE", newNode, index });
   }
 
   if (currentPatch.length > 0) {
     patches.push({ index, changes: currentPatch });
   }
 }
+
 function isTextNode(node) {
-  return node && node.type === 'text';
+  return node && node.type === "text";
 }
+
 function diffAttrs(oldAttrs = {}, newAttrs = {}) {
   const patches = {};
 
-  // Changements ou ajouts
-  for (let key in newAttrs) {
+  for (const key in newAttrs) {
     if (newAttrs[key] !== oldAttrs[key]) {
-      patches[key] = newAttrs[key]; 
+      patches[key] = newAttrs[key];
     }
   }
 
-  // Suppressions
-  for (let key in oldAttrs) {
+  for (const key in oldAttrs) {
     if (!(key in newAttrs)) {
       patches[key] = null;
     }
@@ -61,11 +55,20 @@ function diffAttrs(oldAttrs = {}, newAttrs = {}) {
 
   return patches;
 }
+
 function diffChildren(oldChildren, newChildren, patches, parentIndex) {
   const maxLen = Math.max(oldChildren.length, newChildren.length);
 
+  
+  
+  let currentIndex = parentIndex;
+
   for (let i = 0; i < maxLen; i++) {
-    walk(oldChildren[i], newChildren[i], patches, ++globalIndex);
+    currentIndex++;
+
+    walk(oldChildren[i], newChildren[i], patches, currentIndex);
+
   }
 }
+
 
