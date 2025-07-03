174a70c78ec5418f619ca6cf24a7ba87059aec99
walid-zouguagh <walid.zouguaagh@gmail.com>
2025-07-03 12:49:14 +0100

    app.js

--- examples/todomvc/app.js
+++ examples/todomvc/app.js
@@ -1,46 +1,47 @@
-import { EventListener } from "../../core/events.js";
-import { createHTML, root } from "../../core/dom.js";
+
 import { Router } from "../../core/router.js";
-import { UpdateDOM } from "../../core/Vdom/diffing.js";
-import { useState } from "../../core/state.js";
+import { createHTML, jsx, createElement } from "/core/dom.js";
+import { render } from "../../core/Vdom/render.js";
 
-const rout = new Router();
-
-rout.addrout("/", HomeHandler);
-rout.addrout("/about", () => console.log("hello this is the about page"));
-
-function HomeHandler(req, res) {
-  navbar();
-}
-
-function navbar() {
-  const navbar = createHTML(
-    "nav",
-    { className: "nav", id: "navb" },
-    createHTML(
-      "p",
-      {
-        className: "logo",
-        onclick: () => {
-          let name = "walid";
-          console.log("use State----------", useState(name));
-        },
-      },
-      "test"
-    ),
-    createHTML(
-      "ul",
-      { className: "links" },
-      createHTML("li", {}, createHTML("a", { href: "/" }, "home")),
-      createHTML("li", {}, createHTML("a", { href: "/about" }, "about"))
+function Footer() {
+  return jsx("footer", { className: "info" },
+    jsx("p", {}, "Double-click to edit a todo"),
+    jsx("p", {}, "Created by the mini framework Team"),
+    jsx("p", {},
+      jsx("a", { href: "http://todomvc.com" }, "TodoMVC")
     )
   );
-  //   EventListener("#navb", "click", handleclick);
-  UpdateDOM(navbar);
 }
 
+function App() {
+  return jsx("section", { className: "todoapp", id: "root" },
+    jsx("header", { className: "header", "data-testid": "header" },
+      jsx("h1", {}, "todos"),
+      jsx("div", { className: "input-container" },
+        jsx("input", {
+          className: "new-todo",
+          id: "todo-input",
+          type: "text",
+          "data-testid": "text-input",
+          placeholder: "What needs to be done?",
+        }),
+        jsx("label", {
+          className: "visually-hidden",
+          htmlFor: "todo-input"
+        }, "New Todo Input")
+      )
+    ),
+    jsx("main", { className: "main", "data-testid": "main" },
+      jsx("div", { className: "toggle-all-container" })
+    ),
+    jsx(Footer, {})
+  );
+}
+
+const rout = new Router();
+rout.addrout("/", App);
+rout.addrout("/about", () => console.log("hello this is the about page"));
 rout.handleRouteChange();
 
-// function handleclick(e) {
-//   console.log(e);
-// }
+render(App);
+
